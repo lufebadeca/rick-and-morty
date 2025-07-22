a71efb3474f6226316333d8fd4c27b913b5de369
Luis <lufebadeca@gmail.com>
2025-07-21 20:11:50 -0500

    estilos arreglados para scroll. Historial funcionando para conversación coherente

--- app/chat/[id]/page.js
+++ app/chat/[id]/page.js
@@ -4,8 +4,8 @@ import { Header } from "@/componentes/Header";
 import { Footer } from "@/componentes/Footer";
 import { useParams } from "next/navigation"; //lee los params de la ruta
 import { ListaPersonajes } from "@/componentes/ListaPersonajes";
-import { useState, useEffect } from "react";
-import {GoogleGenAI} from "@google/genai"
+import { useState, useEffect, useRef } from "react";
+import { GoogleGenAI} from "@google/genai"
 
 const ai = new GoogleGenAI( {apiKey: process.env.NEXT_PUBLIC_GEMINI_API_KEY} );
 
@@ -21,6 +21,27 @@ export default function Chat() {
   const params = useParams(); // useParams lee el id incrustado en la URL de la ruta: ej: { id: 7 }
   const idActivo = params.id;  // el id traido de params: ej: 7
   //console.log(idActivo);
+
+  const [chatHistorialRef, setChatHistorialRef] = useState(null);
+
+  //ref para ubicar el final del chat
+  const [loader, setLoader] = useState(false);
+  const chatWindowRef = useRef(null);
+  useEffect(()=>{
+    
+    const chatInstance = ai.chats.create({
+    model: "gemini-2.5-flash",
+    config: {
+      systemInstruction: `Eres una persona llamada ${personajeActivo?.name}, tu género es ${personajeActivo?.gender}, tu origen es ${personajeActivo?.origin.name}`
+    },
+    history: conversacion.map( (msg)=>
+      ( { role: msg.remitente === "yo" ? "user" : "model", parts: [{text: msg.texto}] } )
+    )});
+    setChatHistorialRef(chatInstance);
+
+    chatWindowRef.current?.scrollIntoView({behavior: "smooth"});
+
+  }, [conversacion])
   
   ////este use effect trae los 20 personajes con fetch en la primera carga del componente
   useEffect( ()=>{
@@ -55,38 +76,39 @@ export default function Chat() {
     const nuevoMensaje = {remitente: "yo", texto: textoInput};
     
     setConversacion((prev) => { return [...prev, nuevoMensaje] });
-    const response = await geminiResponse(textoInput);
+    setLoader(true);
+    setTextoInput("");
+    const response = await geminiResponse(nuevoMensaje.texto);
     const mensajeRespta = {remitente: "gemini", texto: response}
     setConversacion((prev) => { return [...prev, mensajeRespta] });
-    setTextoInput("");
+    setLoader(false);
     console.log(conversacion);
   }
 
   const geminiResponse = async (prompt)=>{
-    const response = await ai.models.generateContent({
-      model: "gemini-2.5-flash",
-      contents: prompt
+    const response = await chatHistorialRef.sendMessage({
+      message: prompt,
     });
     console.log(response);
     return response.text;
   }
 
   return (
-    <div className="flex flex-col h-max">
+    <div className="flex flex-col h-screen">
       <Header/>
 
-      <div className="flex h-auto">
+      <div className="flex flex-1 overflow-hidden">
 
         <ListaPersonajes personajes={personajes}/>
 
-        <section className="flex flex-1 flex-col py-4 text-gray-700 space-y-2 overflow-y-auto">
+        <section className="flex flex-col flex-1 py-4 text-gray-700 space-y-2 ">
 
           <article className="flex justify-between p-4 bg-blue-200">
             <p>Conversa con {personajeActivo?.name}</p>
             <img src={personajeActivo?.image} className="w-[30px] rounded-full"></img>
           </article>
 
-          <section className="flex flex-col flex-1 p-4 bg-white rounded-lg shadow-md">
+          <section className="flex flex-col flex-1 p-4 bg-white rounded-lg shadow-md overflow-y-auto">
             {conversacion.map( (mensaje, index)=> 
               <p key={index} className={`p-2 m-1 rounded-lg w-3/4 ${mensaje.remitente === "yo" ? 
                     "bg-blue-500 text-white self-end" : 
@@ -95,6 +117,7 @@ export default function Chat() {
                 {mensaje.texto}
               </p>
             )}
+            { loader && <p id="endchatref" ref={chatWindowRef}>Escribiendo...</p>}
           </section>
 
           <div className="flex gap-6 px-4 py-1">
